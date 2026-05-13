e66c90a282e7c9f4ef35fd77958973b2a462f159
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-12 18:02:50 -0600

    update

--- bash/is-ma.sh
+++ bash/is-ma.sh
@@ -4,5 +4,5 @@ source ~/is-ma/dotfiles/bash/scripts/bash_colors.sh
 source ~/is-ma/dotfiles/bash/scripts/ps1.sh
 
 ### IS-MA: GIT SHORTCUTS ###
-source ~/is-ma/dotfiles/bash/scripts/git_shortcuts.sh
+source ~/is-ma/dotfiles/bash/scripts/shortcuts_for_git.sh
 
