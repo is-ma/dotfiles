c54cb27ef2f9584a59d501da7f6093d5155eb629
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-14 17:00:51 -0600

    update

--- aliaz/mods/linux_only.sh
+++ aliaz/mods/linux_only.sh
@@ -1,16 +1,10 @@
-# Linux configs
-sudo apt-get install -y plocate > /dev/null 2>&1  # alias: loc
-
-# Bash only aliases
-alias alib='cat ~/.is-ma/aliaz/mods/linux_only.sh && cat ~/.is-ma/aliaz/mods/any_os.sh'
-alias be="vi ~/.bashrc"
-alias bs="source ~/.bashrc"
-alias ce='crontab -l > $HOME/my_crontab.conf && vi $HOME/my_crontab.conf && echo "Install changes with: ci."'
+# debian_hub
 alias ci='crontab $HOME/my_crontab.conf && crontab -l'
 alias cl="crontab -l > $HOME/my_crontab.conf && cat $HOME/my_crontab.conf"
-alias e="exit"
-alias g="goaccess"
-alias loc="locate / | egrep -i" # Linux update db: sudo updatedb
-alias mailrm="rm /var/mail/deploy"
-alias mailvi="vi /var/mail/deploy"
+
+# maybe
 replace () { find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s@$1@$2@g"; }
+
+# server_dotfiles
+alias mailrm="rm /var/mail/deploy"
+alias mailww="vi /var/mail/deploy"
