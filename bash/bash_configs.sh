bc1aaf246b814c5397a266848f5d3d5a820fa600
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-15 07:32:41 -0600

    update

--- bash/bash_configs.sh
+++ bash/bash_configs.sh
@@ -1,8 +1,8 @@
 # bash stuff
 HISTCONTROL=ignoreboth       # don't put duplicate lines in history
-HISTFILESIZE=10000           # now we can remember 10000 commands [HDD] (not 2000)
-HISTSIZE=5000                # now we can remember 5000 commands [RAM] (not 1000)
-PROMPT_COMMAND='history -a'  # immediately persist commands to my ~/.bash_history
+HISTFILESIZE=20000           # remember 10000 commands [HDD]
+HISTSIZE=1000                # remember 5000 commands [RAM]
+PROMPT_COMMAND='history -a'  # persist commands to my ~/.bash_history
 set -o vi                    # edit command-line with VI
 
 # tools
