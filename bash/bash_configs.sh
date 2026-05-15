4531490b467fd3ef5669cc839135d7e734fac839
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-15 10:48:35 -0600

    update

--- bash/bash_configs.sh
+++ bash/bash_configs.sh
@@ -2,7 +2,8 @@
 HISTCONTROL=ignoreboth       # don't put duplicate lines in history
 HISTFILESIZE=20000           # remember 10000 commands [HDD]
 HISTSIZE=1000                # remember 5000 commands [RAM]
-PROMPT_COMMAND='history -a'  # persist commands to my ~/.bash_history
+PROMPT_COMMAND="history -a"  # persist commands to my ~/.bash_history
+export EDITOR="vim"          # use it as default (ex. git)
 set -o vi                    # edit command-line with VI
 
 # tools
